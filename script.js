f5ea4099d0ba3cc52b4644c589f1c27ce41395e1
Yan Mesko <yanmesko@gmail.com>
2025-09-16 20:40:56 -0300

    Melhoria do funcionamento e ajuste para resize da window

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dvd = document.getElementById('dvd');
-    const body = document.body
+    const bg = document.getElementById('bg');
 
-    let posX = 0;
-    let posY = 0;
+    let posX = Math.floor(Math.random() * (bg.clientWidth - dvd.clientWidth));
+    let posY = Math.floor(Math.random() * (bg.clientHeight - dvd.clientHeight));
 
     let velX = 2;
     let velY = 2;
 
+    window.addEventListener('resize', () => {
+        const newHeight = bg.clientHeight;
+        const newWidth = bg.clientWidth;
+
+        if (posX + dvd.clientWidth > newWidth){
+            posX = newWidth-dvd.clientWidth;
+        }
+        if (posY + dvd.clientHeight > newHeight){
+            posY = newHeight-dvd.clientHeight;
+        }
+        if(posX<0){
+            posX=0;
+        }
+        if(posY<0){
+            posY=0;
+        }
+
+    })
+
     function animacao(){
         const dvdWidth = dvd.clientWidth;
         const dvdHeight = dvd.clientHeight;
-        const width = body.clientWidth;
-        const height = body.clientHeight;
+        const width = bg.clientWidth;
+        const height = bg.clientHeight;
 
         posX += velX;
         posY += velY;
 
-        if(posX + dvdWidth >= width || posX <= 0){
+        if(posX + dvdWidth >= width || posX <= 0 || posX >=1920){
             velX *= -1;
         }
-        if(posY + dvdHeight >= height || posY <= 0){
+        if(posY + dvdHeight >= height || posY <= 0 || posY >=1080){
             velY *= -1;
         }
         dvd.style.left = `${posX}px`;
